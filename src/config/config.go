bbe8240e15eea3dec8cad312edba67b4c6030e8d
Boris <1016890+denwwer@users.noreply.github.com>
2026-09-22 17:49:59 +0300

    added CF acc id

--- src/config/config.go
+++ src/config/config.go
@@ -5,9 +5,10 @@ var Version = "v0.0.0"
 
 // Config holds environment variable settings.
 type Config struct {
-	Time       string `env:"PB_TIME" required:"true" usage:"Run time, format HH:MM e.g daily at 13:00 or periodic interval - [d]s|m|h|d e.g. every one hour 1h"`
-	DataDir    string `env:"PB_DATA_DIR" default:"/data" usage:"Where store SQLite database"`
-	CFDatabase string `env:"PB_CF_DB_ID" required:"true" usage:"ID of Cloudflare D1 database to store results"`
-	CFD1Token  string `env:"PB_CF_D1_TOKEN" required:"true" usage:"Cloudflare API token with D1 [write] permission"`
-	UrlhausKey string `env:"PB_URLHAUS_KEY" required:"true" usage:"URLhaus auth key"`
+	Time        string `env:"PB_TIME" required:"true" usage:"Run time, format HH:MM e.g daily at 13:00 or periodic interval - [d]s|m|h|d e.g. every one hour 1h"`
+	DataDir     string `env:"PB_DATA_DIR" default:"/data" usage:"Where store SQLite database"`
+	CFDatabase  string `env:"PB_CF_DB_ID" required:"true" usage:"ID of Cloudflare D1 database to store results"`
+	CFAccountID string `env:"PB_CF_ACCOUNT_ID" required:"true" usage:"Cloudflare account ID"`
+	CFD1Token   string `env:"PB_CF_D1_TOKEN" required:"true" usage:"Cloudflare API token with D1 [write] permission"`
+	UrlhausKey  string `env:"PB_URLHAUS_KEY" required:"true" usage:"URLhaus auth key"`
 }
